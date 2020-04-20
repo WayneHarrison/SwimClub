0a72fa37d7aa7c69e8f596f0248829ba98b6c91e
WayneHarrison <WayneJnr@planetwayne.com>
2020-04-20 15:46:16 +0100

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,10 +1,10 @@
 <?php
 
 //connect to mysql database
-$servername = "localhost";
-  $username = "wh01";
-  $password = "Kauqylw7tpP1FNMt";
-  $databasename = "swimclub";
+$servername = "b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+  $username = "m0a8ocouho4elhr3";
+  $password = "fbhf47zimyqaf4kn";
+  $databasename = "tqxi9rz9zqkfnnhw";
 
   $conn =  mysqli_connect($servername, $username, $password,$databasename);
   // Check connection
